1f5cc26c16f3f31c148bcb3dc3e70d9eb767b2a3
Arqam Ahmed <arqam911204@gmail.com>
2025-08-12 15:34:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -358,7 +358,7 @@ function App() {
                 <img src="/images/college-companion.jpeg" alt="College Companion" />
                 <div className="project-overlay">
                   <div className="project-links">
-                    <a href="https://college-companion-by-imad.netlify.app/" target="_blank" className="project-link live">
+                    <a href="https://college-companion-by-arqam.netlify.app/" target="_blank" className="project-link live">
                       <span>Live Demo</span>
                       <svg viewBox="0 0 24 24">
                         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
@@ -397,7 +397,7 @@ function App() {
                         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
                       </svg>
                     </a>
-                    <a href="https://github.com/mohammed-imad-umar/Electronic_Signature-" target="_blank" className="project-link github">
+                    <a href="https://github.com/mohammedarqamahmed/Electronic_Signature-" target="_blank" className="project-link github">
                       <span>GitHub</span>
                       <svg viewBox="0 0 24 24">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
@@ -424,7 +424,7 @@ function App() {
                 <img src="/images/smart-parking-project.png" alt="Smart Parking" />
                 <div className="project-overlay">
                   <div className="project-links">
-                    <a href="https://github.com/mohammed-imad-umar/smart-parking-system" target="_blank" className="project-link github">
+                    <a href="https://github.com/mohammedarqamahmed/smart-parking-system" target="_blank" className="project-link github">
                       <span>GitHub</span>
                       <svg viewBox="0 0 24 24">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
@@ -451,7 +451,7 @@ function App() {
                 <img src="/images/twitter-bots-project.png" alt="Twitter Bots" />
                 <div className="project-overlay">
                   <div className="project-links">
-                    <a href="https://github.com/mohammed-imad-umar/detecting-twitter-bots" target="_blank" className="project-link github">
+                    <a href="https://github.com/mohammedarqamahmed/detecting-twitter-bots" target="_blank" className="project-link github">
                       <span>GitHub</span>
                       <svg viewBox="0 0 24 24">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
